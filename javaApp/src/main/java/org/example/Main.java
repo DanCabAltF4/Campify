9aacc9342cb3cffacf05a17741d74f1e7122d466
paco11111 <152623847+paco11111@users.noreply.github.com>
2025-12-16 19:03:00 +0100

    Menu

--- javaApp/src/main/java/org/example/Main.java
+++ javaApp/src/main/java/org/example/Main.java
@@ -1,18 +1,37 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.example.persistence.ConexionDB;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        EntityManagerFactory emf = ConexionDB.getInstance().getConexion().getEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
 
+        int opcion;
+        do {
+            System.out.println("1.");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("0. Salir");
+            System.out.print("Opción: ");
+            opcion = new Scanner(System.in).nextInt();
+            switch (opcion) {
+                // case 1 -> ;
+                // case 2 -> ;
+                // case 3 -> ;
+                // case 4 -> ;
+                // case 0 -> System.out.println("Saliendo...");
+                // default -> System.out.println("Opción no válida.");
+            }
+        } while (opcion != 0);
+        // ConexionDB.CerrarConexion(emf);
     }
 }
