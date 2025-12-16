43b351eba725a921c0d537c0d9d074b83371a5fa
a <minglesg2401@educantabria.es>
2025-12-16 16:15:57 +0100

    -Cambios cliente

--- src/main/java/org/example/model/Cliente.java
+++ src/main/java/org/example/model/Cliente.java
@@ -1,17 +1,23 @@
 package org.example.model;
 
-import javax.annotation.processing.Generated;
+import jakarta.persistence.*;
 
-@Entity
+import javax.annotation.processing.Generated;
+import java.time.LocalDateTime;
+
+@Entity(name = "clientes")
 public class Cliente {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    @Column(length = 50)
     private String nombre;
+    @Column(length = 100)
     private String apellidos;
+    @Column(length = 9)
     private String dni;
     private String direccion;
     private String cPostal;
-    private DateTime fechaNacimiento;
+    private LocalDateTime fechaNacimiento;
 }
