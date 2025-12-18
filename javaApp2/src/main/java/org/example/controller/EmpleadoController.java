c0abb99a09fe0bd9f75484681792e922b21c4fb4
Daniel <dcabezab18@gmail.com>
2025-12-18 18:05:58 +0100

    +Funcionamiento API para Empleados

---
+++ javaApp2/src/main/java/org/example/controller/EmpleadoController.java
@@ -0,0 +1,36 @@
+package org.example.controller;
+
+import org.example.model.Empleado;
+import org.example.service.ServiceEmpleado;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/empleados")
+class EmpleadoController {
+
+    private ServiceEmpleado service;
+
+    public EmpleadoController(ServiceEmpleado service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Empleado insert(@RequestBody Empleado empleado){
+        return service.insert(empleado);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        service.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public Empleado update(@PathVariable int id, @RequestBody Empleado empleado){
+        return service.update(id,empleado);
+    }
+
+    @GetMapping("/{id}")
+    public Empleado findById(@PathVariable int id){
+        return service.findById(id);
+    }
+}
---
+++ javaApp2/src/main/java/org/example/service/IServiceEmpleado.java
@@ -0,0 +1,6 @@
+package org.example.service;
+
+import org.example.model.Empleado;
+
+public interface IServiceEmpleado extends IService<Empleado, Integer>{
+}
