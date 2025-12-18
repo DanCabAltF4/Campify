7b1aeda65809c2b12200b3eae6343a23171f9935
MaxwellRoyers <oriolfs2005@gmail.com>
2025-12-18 16:19:05 +0100

    M - Añadidos controller, service y repository de Parcelas. Eliminada la clase Main por no ser necesaria y añadido parcelas.http para probar los métodos GET, POST, PUT y DELETE

---
+++ javaApp2/src/main/java/org/example/service/ServiceParcela.java
@@ -0,0 +1,50 @@
+package org.example.service;
+
+import org.example.model.Parcela;
+import org.example.persistence.ParcelaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceParcela implements  IServiceParcela{
+
+    private ParcelaRepository repo;
+
+    public ServiceParcela(ParcelaRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public Parcela insert(Parcela parcela) {
+        return repo.save(parcela);
+    }
+
+    @Override
+    public Parcela update(int id, Parcela parcela) {
+        Parcela buscada = findById(id);
+        if(buscada!=null){
+            buscada.setEstado_parcela(parcela.getEstado_parcela());
+            buscada.setTipo_parcela(parcela.getTipo_parcela());
+            buscada.setCerca_baño(parcela.isCerca_baño());
+            buscada.setTiene_vistas(parcela.isTiene_vistas());
+            buscada.setCapacidad_maxima(parcela.getCapacidad_maxima());
+            buscada.setPrecio_noche(parcela.getPrecio_noche());
+            buscada.setZona_sombra(parcela.isZona_sombra());
+            repo.save(buscada);
+        }
+        return buscada;
+    }
+
+    @Override
+    public void delete(int id) {
+        Parcela buscada = findById(id);
+        if(buscada!=null){
+            repo.delete(buscada);
+        }
+    }
+
+    @Override
+    public Parcela findById(int id) {
+        Parcela parcela = repo.findById(id).orElse(null);
+        return parcela;
+    }
+}
